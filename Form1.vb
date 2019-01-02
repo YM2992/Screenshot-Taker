f4dee2657505816787223b1969ebfb68a8c2975d
YM2992 <46312028+YM2992@users.noreply.github.com>
2019-01-02 18:13:31 +1100

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,149 @@
+﻿Imports System.Text
+
+Public Class Form1
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vkey As Long) As Integer
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        Dim ctrlkey As Boolean
+        Dim shiftkey As Boolean
+        Dim o As Boolean
+
+        ctrlkey = GetAsyncKeyState(Keys.ControlKey)
+        shiftkey = GetAsyncKeyState(Keys.ShiftKey)
+        o = GetAsyncKeyState(Keys.O)
+
+        If ctrlkey And o = True Then
+            Button1.PerformClick()
+        End If
+    End Sub
+
+    Dim drag As Boolean
+    Dim mousex As Integer
+    Dim mousey As Integer
+
+    Private Sub Panel1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseDown
+        drag = True
+        mousex = Windows.Forms.Cursor.Position.X - Me.Left
+        mousey = Windows.Forms.Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Panel1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseMove
+        If drag Then
+            Me.Top = Windows.Forms.Cursor.Position.Y - mousey
+            Me.Left = Windows.Forms.Cursor.Position.X - mousex
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub Label1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Label1.MouseDown
+        drag = True
+        mousex = Windows.Forms.Cursor.Position.X - Me.Left
+        mousey = Windows.Forms.Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Label1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Label1.MouseMove
+        If drag Then
+            Me.Top = Windows.Forms.Cursor.Position.Y - mousey
+            Me.Left = Windows.Forms.Cursor.Position.X - mousex
+        End If
+    End Sub
+
+    Private Sub Label1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Label1.MouseUp
+        drag = False
+    End Sub
+
+    Dim count As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'MsgBox("No folder directory selected, please select one to continue")
+        count = count + 1
+
+        Dim screenSize As Size = New Size(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+        Dim screenGrab As New Bitmap(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+        Dim g As Graphics = Graphics.FromImage(screenGrab)
+        g.CopyFromScreen(Point.Empty, Point.Empty, screenSize)
+        Dim img = New Bitmap(screenGrab)
+        img.Save(FolderBrowserDialog1.SelectedPath & "\\screenshot_" & GeneratePassword() & ".png", Imaging.ImageFormat.Png)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            My.Settings.ModFolder = FolderBrowserDialog1.SelectedPath
+            My.Settings.Save()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Exit()
+        Environment.Exit(1)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FolderBrowserDialog1.SelectedPath = My.Settings.ModFolder
+        count = My.Settings.ScreenshotCount
+        Timer1.Enabled = True
+        Timer1.Interval = 1
+        Hotkey.registerHotkey(Me, "O", Hotkey.KeyModifier.Control)
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As EventArgs) Handles MyBase.Closing
+        My.Settings.ScreenshotCount = count
+        My.Settings.Save()
+    End Sub
+
+    Function GeneratePassword()
+        Dim s As String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        Dim r As New Random
+        Dim sb As New StringBuilder
+        For i As Integer = 1 To 48
+            Dim idx As Integer = r.Next(0, 35)
+            sb.Append(s.Substring(idx, 1))
+        Next
+        Return sb.ToString()
+    End Function
+End Class
+
+Public Class Hotkey
+
+#Region "Declarations - WinAPI, Hotkey constant and Modifier Enum"
+    ''' <summary>
+    ''' Declaration of winAPI function wrappers. The winAPI functions are used to register / unregister a hotkey
+    ''' </summary>
+    Private Declare Function RegisterHotKey Lib "user32" _
+        (ByVal hwnd As IntPtr, ByVal id As Integer, ByVal fsModifiers As Integer, ByVal vk As Integer) As Integer
+
+    Private Declare Function UnregisterHotKey Lib "user32" (ByVal hwnd As IntPtr, ByVal id As Integer) As Integer
+
+    Public Const WM_HOTKEY As Integer = &H312
+
+    Enum KeyModifier
+        None = 0
+        Alt = &H1
+        Control = &H2
+        Shift = &H4
+        Winkey = &H8
+    End Enum 'This enum is just to make it easier to call the registerHotKey function: The modifier integer codes are replaced by a friendly "Alt","Shift" etc.
+#End Region
+
+
+#Region "Hotkey registration, unregistration and handling"
+    Public Shared Sub registerHotkey(ByRef sourceForm As Form, ByVal triggerKey As String, ByVal modifier As KeyModifier)
+        RegisterHotKey(sourceForm.Handle, 1, modifier, Asc(triggerKey.ToUpper))
+    End Sub
+    Public Shared Sub unregisterHotkeys(ByRef sourceForm As Form)
+        UnregisterHotKey(sourceForm.Handle, 1)  'Remember to call unregisterHotkeys() when closing your application.
+    End Sub
+    Public Shared Sub handleHotKeyEvent(ByVal hotkeyID As IntPtr)
+        MsgBox("The hotkey was pressed")
+    End Sub
+#End Region
+
+End Class
